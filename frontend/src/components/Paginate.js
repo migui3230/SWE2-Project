19d26627258bcef04f433b5b035726581d1cbc96
Carolina Fernandez <carolina26fs@gmail.com>
2023-04-09 17:20:26 -0400

    Extract ternary operation from paginate func, Fixes #16

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -3,19 +3,26 @@ import { Pagination } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
 const Paginate = ({ pages, page, isAdmin = false, keyword = '' }) => {
+
+  // fixes issue #16 
+  const pageLink = (x) => {
+    if (!isAdmin) {
+      if (keyword) {
+        return `/search/${keyword}/page/${x + 1}`
+      } else {
+        return `/page/${x + 1}`
+      }
+    } else {
+      return `/admin/productlist/${x + 1}`
+    }
+  }
   return (
     pages > 1 && (
       <Pagination>
         {[...Array(pages).keys()].map((x) => (
           <LinkContainer
             key={x + 1}
-            to={
-              !isAdmin
-                ? keyword
-                  ? `/search/${keyword}/page/${x + 1}`
-                  : `/page/${x + 1}`
-                : `/admin/productlist/${x + 1}`
-            }
+            to={pageLink()}
           >
             <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
           </LinkContainer>
