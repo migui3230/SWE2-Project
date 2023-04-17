3fedd97269044c2f977f65415434dc11ca89e9e0
Miguel Sarenas <migui0323@gmail.com>
2023-04-16 21:18:37 -0400

    fix backend errors

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,5 @@ const PORT = process.env.PORT || 3001;
 
 app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-  )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
