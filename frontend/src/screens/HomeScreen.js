4068d1ff2abeb7904d3f1e01c766cfbe058af2d9
Carolina Fernandez <carolina26fs@gmail.com>
2023-04-28 01:50:55 -0400

    Added category filter, Closes #59

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Dropdown } from "react-bootstrap";
 import Product from "../components/Product";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
@@ -25,23 +25,35 @@ const HomeScreen = ({ match }) => {
   const [currentProducts, setCurrentProducts] = useState([]);
   const [lowerPriceBoundary, setLowerPriceBoundary] = useState(0);
   const [upperPriceBoundary, setUpperPriceBoundary] = useState(1000);
+  const [categoryFilter, setCategoryFilter] = useState(null);
 
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber));
   }, [dispatch, keyword, pageNumber]);
 
-  // update currentProducts when they are loaded in from Redux
+  // update currentProducts and categories when they are loaded in from Redux
   useEffect(() => {
     setCurrentProducts(products);
   }, [products])
 
-  const handleFilterClicked = () => {
+  useEffect(() => {
+    if (categoryFilter != null) {
+      const filteredProductList = currentProducts.filter(
+        (product) => product.category === categoryFilter
+      );
+      setCurrentProducts(filteredProductList);
+    } else {
+      handlePriceFilterClicked();
+    }
+
+  }, [categoryFilter])
+
+  const handlePriceFilterClicked = () => {
     const filteredProductList = products.filter(
       (product) => product.price >= lowerPriceBoundary && product.price <= upperPriceBoundary
     );
     setCurrentProducts(filteredProductList);
   }
-
 
   const renderLoadingOrProducts = () => {
     if (loading) {
@@ -71,9 +83,10 @@ const HomeScreen = ({ match }) => {
   const renderPriceFilters = () => {
     return (
       <>
-        <Row>
+        <Row className="d-flex align-items-center">
           <Col sm={12} md={6} lg={4} xl={3} className="d-flex align-items-center">
-            <h3>Filter by Price</h3></Col>
+            <h3>Filter by Price</h3>
+          </Col>
           <Col sm={12} md={6} lg={4} xl={3} className="d-flex align-items-center">
             <input type="numeric" placeholder="Min" value={lowerPriceBoundary} onChange={(e) => setLowerPriceBoundary(e.target.value)} />
           </Col>
@@ -81,8 +94,39 @@ const HomeScreen = ({ match }) => {
             <input type="numeric" placeholder="Max" value={upperPriceBoundary} onChange={(e) => setUpperPriceBoundary(e.target.value)} />
           </Col>
           <Col sm={12} md={6} lg={4} xl={3} className="d-flex align-items-center">
-            <button className="btn btn-light" onClick={handleFilterClicked}>Filter</button>
+            <button className="btn btn-dark" onClick={handlePriceFilterClicked}>Filter</button>
           </Col>
+        </Row>
+      </>
+    )
+  }
+
+  const renderCategoryFilters = () => {
+    const categories = Array.from([...new Set(products.map((prod) => prod.category))]);
+    return (
+      <>
+        <Row className="d-flex align-items-center">
+          <Col sm={12} md={6} lg={4} xl={3} className="d-flex align-items-center">
+            <h3>Filter by Category</h3>
+          </Col>
+          <Dropdown onSelect={(eKey, e) => setCategoryFilter(eKey)}>
+            <Dropdown.Toggle variant='secondary' id='category-filter-dropdown'>
+              Category
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              {categories.map((cat, i) => {
+                return (
+                  <Dropdown.Item
+                    key={i}
+                    eventKey={cat}
+                  >
+                    {cat}
+                  </Dropdown.Item>
+                )
+              })
+              }
+            </Dropdown.Menu>
+          </Dropdown>
         </Row>
       </>
     )
@@ -99,7 +143,16 @@ const HomeScreen = ({ match }) => {
           </Link>
         )}
       {renderPriceFilters()}
+      {renderCategoryFilters()}
       <h1>Latest Products</h1>
+      {categoryFilter &&
+        <Row className="d-flex justify-content-center align-items-center">
+          <button className="category-filter-btn" onClick={() => setCategoryFilter(null)}>
+            &#10005;
+            </button>
+          {categoryFilter}
+        </Row>
+      }
       {renderLoadingOrProducts()}
     </>
   );
