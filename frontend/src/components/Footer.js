d9e6f7f36fcefb709816b8108543bd6fd2ef5aee
Quan Pham <minhquan14092002@gmail.com>
2023-05-04 23:24:51 -0400

    initial set up

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import {Link} from {}
 
 const Footer = () => {
   return (
     <footer>
       <Container>
         <Row>
+          <Col className='text-center py-3'>
+            <Link></Link>
+          </Col>
           <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
         </Row>
       </Container>
